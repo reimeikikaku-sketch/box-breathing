22512aca22596eff1c9c68a73defff2f05b5b223
reimeikikaku-sketch <reimeikikaku@gmail.com>
2026-07-02 20:12:42 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BoxBreathing from '../boxbreathing.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BoxBreathing />
+  </React.StrictMode>
+)
